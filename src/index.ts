6ba42b2f4e32b929566205cbed0f320625763507
João Daniel <jotaf.daniel@gmail.com>
2021-03-05 10:23:16 -0300

    add monitoring with prometheus
    Signed-off-by: João Daniel <jotaf.daniel@gmail.com>

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,33 @@
 import * as Express from 'express';
 import * as proxy from 'express-http-proxy';
+import { Counter, Registry } from 'prom-client';
 
 const port = Number(process.env.PORT) || 3000;
 const host = process.env.HOST || '0.0.0.0';
 
 const app = Express();
 
-app.get('/', proxy('https://themicroservicesinfo.netlify.app'))
+const registry = new Registry();
+
+const counter = new Counter({
+  name: 'incomming_requests',
+  help: 'Counts the total amount of incomming requests to the API Gateway',
+  registers: [registry]
+})
+
+app.get(
+  '/',
+  (_, __, next) => {
+    counter.inc();
+    next();
+  },
+  proxy('https://themicroservicesinfo.netlify.app')
+)
+
+app.get('/metrics', async (_, res) => {
+  res.type(registry.contentType)
+  res.send(await registry.metrics());
+})
 
 app.listen(port, host, () => console.log('server running'));
 
