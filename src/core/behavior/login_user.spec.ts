aeb99a2ee495d8cd663571c6801e96eedd77680f
João Daniel <jotaf.daniel@gmail.com>
2021-03-06 17:33:19 -0300

    add tests for database and login
    Signed-off-by: João Daniel <jotaf.daniel@gmail.com>

---
+++ src/core/behavior/login_user.spec.ts
@@ -0,0 +1,45 @@
+import { LoginUser } from './login_user';
+
+describe(LoginUser, () => {
+  let login;
+  let mockService;
+
+  beforeEach(() => {
+    mockService = { findByEmail: jest.fn() };
+    login = new LoginUser(mockService)
+  })
+
+  it('returns false when user not found', async () => {
+    mockService.findByEmail.mockRejectedValueOnce("User not found");
+
+    expect(await login.run("email@mail.com", "password")).toBeFalsy();
+  })
+
+  it('returns false when password mismatch', async () => {
+    const email = 'email@example.com';
+
+    mockService.findByEmail.mockResolvedValueOnce({
+      id: '1',
+      name: 'Example',
+      email,
+      passwordDigest: '123456'
+    });
+
+    expect(await login.run(email, 'abcdef')).toBeFalsy();
+  })
+
+  it('retuns true when email and password matches', async () => {
+    const email = 'email@example.com';
+    const passwordDigest = '123456';
+
+    mockService.findByEmail.mockResolvedValueOnce({
+      id: '1',
+      name: 'Example',
+      email,
+      passwordDigest,
+    });
+
+    expect(await login.run(email, passwordDigest)).toBeTruthy();
+  })
+})
+
---
+++ src/database/in_memory_user_data_service.spec.ts
@@ -0,0 +1,43 @@
+import { InMemoryUserDataservice } from './in_memory_user_data_service';
+
+describe(InMemoryUserDataservice, () => {
+  let ds: InMemoryUserDataservice;
+
+  beforeEach(() => {
+    ds = new InMemoryUserDataservice();
+  })
+
+  describe('storeOne', () => {
+    it('stores properly a user', async () => {
+      const newUser = { id: '1', name: 'foo', email: 'foo@mail.com', passwordDigest: '123456' };
+      await ds.storeOne(newUser);
+
+      expect(ds.users).toEqual([newUser])
+    });
+  })
+
+  describe('findByEmail', () => {
+
+    it('throws when user not found', async () => {
+      ds.users.find = jest.fn().mockReturnValueOnce(undefined);
+
+      expect.assertions(1);
+
+      try {
+        await ds.findByEmail("foo@mail.com");
+      } catch (e) {
+        expect(e).toEqual("User not found");
+      }
+    })
+
+    it('returns the user, when it is found', async () => {
+      const user = { id: '1', name: 'foo', email: 'foo@mail.com', passwordDigest: '123456' };
+
+      ds.users = [user]
+
+      const userFound = await ds.findByEmail(user.email);
+
+      expect(userFound).toEqual(user);
+    })
+  })
+})
