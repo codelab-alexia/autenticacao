fc6a9ee14b41ae0c24319f76175b537fe3692626
João Daniel <jotaf.daniel@gmail.com>
2021-03-12 11:02:27 -0300

    add dir for e2e tests and yarn scripts
    Signed-off-by: João Daniel <jotaf.daniel@gmail.com>

--- jest.config.js
+++ jest.config.js
@@ -1,9 +1,9 @@
 module.exports = {
-  roots: ['<rootDir>/src'],
+  roots: ['<rootDir>/'],
   transform: {
     '^.+\\.tsx?$': 'ts-jest',
   },
-  testRegex: '(/__tests__/.*|(\\.|/)spec)\\.tsx?$',
+  testRegex: '(/e2e-tests/.*|(\\.|/)spec)\\.tsx?$',
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
   globals: {
     'ts-jest': {
